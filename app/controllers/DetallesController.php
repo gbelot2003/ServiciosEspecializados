a48387b2a393879eb53b439aa21a879b270bda95
= <=>
2014-12-03 11:40:15 +0000

    Cleaning the Controllers of null clasess

--- app/controllers/DetallesController.php
+++ app/controllers/DetallesController.php
@@ -15,28 +15,6 @@ class DetallesController extends \BaseController {
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 * GET /detalles/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /detalles
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
 	 * Display the specified resource.
 	 * GET /detalles/{id}
 	 *
@@ -49,40 +27,6 @@ class DetallesController extends \BaseController {
 		return $detalle;
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /detalles/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /detalles/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /detalles/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
 
 }
--- app/controllers/PortafoliosController.php
+++ app/controllers/PortafoliosController.php
@@ -16,28 +16,6 @@ class PortafoliosController extends \BaseController {
 	}
 	
 	/**
-	 * Show the form for creating a new resource.
-	 * GET /portafolios/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /portafolios
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
 	 * Display the specified resource.
 	 * GET /portafolios/{id}
 	 *
@@ -50,40 +28,5 @@ class PortafoliosController extends \BaseController {
 		return $portafolio;
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /portafolios/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /portafolios/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /portafolios/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
 
 }
