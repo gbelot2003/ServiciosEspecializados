e52e17aac064326138dd5c3b79ca5b694b7c6268
= <=>
2014-12-04 01:53:46 +0000

    correcting template and buttons effects

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -6,6 +6,13 @@
 			<div class="col s12 l4"></div>
 			<div class="col s12">
 				<div class="text-center">
+					<div class="row">
+						<div class="container">
+					      <div class="commit">
+
+					      </div>
+						</div>
+					</div>
 					<div class="credit">Copyright © 2014 Servicios Especializados <br></div>	
 				</div>
 			</div>
